5f3b1a40744856328355fa63ffec9416e0bc25ce
Rawan Alharthi <s43800023@students.tu.edu.sa>
2023-01-10 03:02:25 +0300

    Initial Commit

---
+++ Grocery-List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grocery-List
+//
+//  Created by 라완 💕 on 17/06/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
